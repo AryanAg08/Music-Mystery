50aeb9d004323685629a382a02b0e0ab23817cb9
AryanAg08 <aryangoyal90@outlook.com>
2024-01-15 19:51:31 +0530

    Foldering structures

---
+++ Frontend/symphonic-secrets/src/Pages/Landing Page/index.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import "./index.css";
+
+
+export default function LandingPage() {
+
+
+    return (
+        <div>
+            <h1 className="landing-title">Welcome to the Coding Dojo! </h1>
+        </div>
+    )
+}
